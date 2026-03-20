eb8edfc9586a5e155a7371de9047a107ba05daa8
Chris Le Marquand <chris.lemarquand@gmail.com>
2026-03-20 14:10:06 +0000

    chore: enforce local-only SharedUI lockstep workflow

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .executable(name: "ExifEditMac", targets: ["ExifEditMac"])
     ],
     dependencies: [
-        .package(url: "https://github.com/chrislemarquand/SharedUI.git", exact: "1.0.0")
+        .package(path: "../SharedUI")
     ],
     targets: [
         .target(name: "ExifEditCore"),
